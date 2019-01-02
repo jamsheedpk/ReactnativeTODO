4ee831d36512a3c13a750c720b620e3053804526
Jamsheed <jamsheedprmc@gmail.com>
2019-01-02 21:53:28 +0530

    ToDo first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import App1 from './1-todoApp-rn';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
